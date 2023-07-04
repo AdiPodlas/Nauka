1affcf3c8389dd27478c74293ae66e31fc60b395
Adrian <a.podlasinski@icloud.com>
2023-07-04 09:26:10 +0200

    Zadania 04.07.2023

---
+++ src/Ksztalty/Main.java
@@ -0,0 +1,78 @@
+package Ksztalty;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Shape triangle = new Triangle(5, 4, 3, 4, 5);
+        Shape rectangle = new Rectangle(6, 8);
+        Shape circle = new Circle(3);
+
+        double totalArea = triangle.calculateArea() + rectangle.calculateArea() + circle.calculateArea();
+        double totalPerimeter = triangle.calculatePerimeter() + rectangle.calculatePerimeter() + circle.calculatePerimeter();
+
+        System.out.println("Total area: " + totalArea);
+        System.out.println("Total perimeter: " + totalPerimeter);
+    }
+    interface Shape {
+        double calculateArea();
+        double calculatePerimeter();
+    }
+
+    static class Triangle implements Shape {
+        private double base;
+        private double height;
+        private double sideA;
+        private double sideB;
+        private double sideC;
+
+        public Triangle(double base, double height, double sideA, double sideB, double sideC) {
+            this.base = base;
+            this.height = height;
+            this.sideA = sideA;
+            this.sideB = sideB;
+            this.sideC = sideC;
+        }
+
+        public double calculateArea() {
+            return 0.5 * base * height;
+        }
+
+        public double calculatePerimeter() {
+            return sideA + sideB + sideC;
+        }
+    }
+
+    static class Rectangle implements Shape {
+        private double length;
+        private double width;
+
+        public Rectangle(double length, double width) {
+            this.length = length;
+            this.width = width;
+        }
+
+        public double calculateArea() {
+            return length * width;
+        }
+
+        public double calculatePerimeter() {
+            return 2 * (length + width);
+        }
+    }
+
+    static class Circle implements Shape {
+        private double radius;
+
+        public Circle(double radius) {
+            this.radius = radius;
+        }
+
+        public double calculateArea() {
+            return Math.PI * radius * radius;
+        }
+
+        public double calculatePerimeter() {
+            return 2 * Math.PI * radius;
+        }
+    }
+}
---
+++ src/Rabat/Discount2.java
@@ -0,0 +1,51 @@
+package Rabat;
+
+public class Discount2 {
+    interface Discount {
+        double calculateDiscountedPrice(double totalPrice);
+    }
+
+    static class FixedDiscount implements Discount {
+        public double calculateDiscountedPrice(double totalPrice) {
+            double discountedPrice = totalPrice - 10;
+            return Math.max(discountedPrice, 0);
+        }
+    }
+
+    static class PercentDiscount implements Discount {
+        public double calculateDiscountedPrice(double totalPrice) {
+            double discountAmount = 0.05 * totalPrice;
+            double discountedPrice = totalPrice - discountAmount;
+            return Math.max(discountedPrice, 0);
+        }
+    }
+
+    static class ConditionalDiscount implements Discount {
+        public double calculateDiscountedPrice(double totalPrice) {
+            if (totalPrice > 100) {
+                double discountAmount = 0.1 * totalPrice;
+                double discountedPrice = totalPrice - discountAmount;
+                return Math.max(discountedPrice, 0);
+            } else {
+                return totalPrice;
+            }
+        }
+    }
+
+        public static void main(String[] args) {
+            double totalPrice = 150;
+
+            Discount fixedDiscount = new FixedDiscount();
+            double fixedDiscountedPrice = fixedDiscount.calculateDiscountedPrice(totalPrice);
+            System.out.println("Fixed discount: " + fixedDiscountedPrice);
+
+            Discount percentDiscount = new PercentDiscount();
+            double percentDiscountedPrice = percentDiscount.calculateDiscountedPrice(totalPrice);
+            System.out.println("Percent discount: " + percentDiscountedPrice);
+
+            Discount conditionalDiscount = new ConditionalDiscount();
+            double conditionalDiscountedPrice = conditionalDiscount.calculateDiscountedPrice(totalPrice);
+            System.out.println("Conditional discount: " + conditionalDiscountedPrice);
+        }
+    }
+
